3e3b6d189a63c5b6835ef6ba0e54b6e3253c8f7e
nadien <sergiomen4@gmail.com>
2016-03-14 02:02:21 -0600

    changes of ui and fuzzyficator

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@
 #include "dialog.h"
 #include "ui_mainwindow.h"
 #include <string>
+#include <sstream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -26,59 +27,68 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
 
+    std::ostringstream ostr;
 
     using namespace fl;
-fl::Engine* engine = new fl::Engine;
-engine->setName("");
 
-fl::InputVariable* inputVariable1 = new fl::InputVariable;
-inputVariable1->setEnabled(true);
-inputVariable1->setName("temperatura");
-inputVariable1->setRange(-20.000, 40.000);
-inputVariable1->addTerm(new fl::Triangle("frio", -20.000, 0.000, 15.000));
-inputVariable1->addTerm(new fl::Triangle("templado", 5.000, 16.000, 27.000));
-inputVariable1->addTerm(new fl::Triangle("caliente", 23.000, 30.000, 40.000));
-engine->addInputVariable(inputVariable1);
+    fl::Engine* engine = new fl::Engine;
+    engine->setName("");
+    int minTemperatura = ui->minTemp->value();
+    int maxTemperatura = ui->maxTemp->value();
+    int minHumedad = ui->minHum->value();
+    int maxHumedad = ui->maxHum->value();
+    int minVelocidad = ui->minVel->value();
+    int maxVelocidad = ui->maxVel->value();
 
-fl::InputVariable* inputVariable2 = new fl::InputVariable;
-inputVariable2->setEnabled(true);
-inputVariable2->setName("humedad");
-inputVariable2->setRange(0.000, 100.000);
-inputVariable2->addTerm(new fl::Triangle("baja", 0.000, 20.000, 40.000));
-inputVariable2->addTerm(new fl::Triangle("media", 35.000, 55.000, 75.000));
-inputVariable2->addTerm(new fl::Triangle("alta", 70.000, 85.000, 100.000));
-engine->addInputVariable(inputVariable2);
+  int  minTempMax = minTemperatura + ( minTemperatura * -1) + 10;
 
-fl::OutputVariable* outputVariable = new fl::OutputVariable;
-outputVariable->setEnabled(true);
-outputVariable->setName("velocidad");
-outputVariable->setRange(0.000, 1000.000);
+    fl::InputVariable* inputVariable1 = new fl::InputVariable;
+    inputVariable1->setEnabled(true);
+    inputVariable1->setName("temperatura");
+    inputVariable1->setRange(minTemperatura, maxTemperatura);
+    inputVariable1->addTerm(new fl::Trapezoid("frio", minTemperatura, minTemperatura, 0 , maxTemperatura * 0.2));
+    inputVariable1->addTerm(new fl::Triangle("templado", maxTemperatura * 0.1 , maxTemperatura * 0.4 , maxTemperatura * 0.7 ));
+    inputVariable1->addTerm(new fl::Trapezoid("caliente", maxTemperatura * 0.6 ,  maxTemperatura * 0.87 , maxTemperatura, maxTemperatura));
+    engine->addInputVariable(inputVariable1);
 
-outputVariable->fuzzyOutput()->setAccumulation(new fl::Maximum);
-outputVariable->setDefuzzifier(new fl::Centroid(200));
-outputVariable->setDefaultValue(fl::nan);
+    fl::InputVariable* inputVariable2 = new fl::InputVariable;
+    inputVariable2->setEnabled(true);
+    inputVariable2->setName("humedad");
+    inputVariable2->setRange(0.000, 100.000);
+    inputVariable2->addTerm(new fl::Trapezoid("baja", minHumedad, minHumedad, maxHumedad * 0.15, maxHumedad * 0.20));
+    inputVariable2->addTerm(new fl::Triangle("media", 20.000, maxHumedad/2, maxHumedad * 0.75));
+    inputVariable2->addTerm(new fl::Trapezoid("alta", maxHumedad * 0.7, maxHumedad * 0.80, maxHumedad, maxHumedad));
+    engine->addInputVariable(inputVariable2);
 
-outputVariable->addTerm(new fl::Triangle("lenta", 0.000, 200.000, 400.000));
-outputVariable->addTerm(new fl::Triangle("media", 300.000, 500.000, 700.000));
-outputVariable->addTerm(new fl::Triangle("rapida", 600.000, 800.000, 1000.000));
-engine->addOutputVariable(outputVariable);
+    fl::OutputVariable* outputVariable = new fl::OutputVariable;
+    outputVariable->setEnabled(true);
+    outputVariable->setName("velocidad");
+    outputVariable->setRange(minVelocidad , maxVelocidad);
+    outputVariable->fuzzyOutput()->setAccumulation(new fl::Maximum);
+    outputVariable->setDefuzzifier(new fl::Centroid(200));
+    outputVariable->setDefaultValue(fl::nan);
+    outputVariable->addTerm(new fl::Trapezoid("baja", 0.000, 0.000, 700.000, 1300.000));
+    outputVariable->addTerm(new fl::Triangle("media", 1100.000, 1800.000, 2600.000));
+    outputVariable->addTerm(new fl::Trapezoid("alta", 2400.000, 3000.000, 3600.000, 3600.000));
+    engine->addOutputVariable(outputVariable);
 
-fl::RuleBlock* ruleBlock = new fl::RuleBlock;
-ruleBlock->setEnabled(true);
-ruleBlock->setName("");
-ruleBlock->setConjunction(new fl::Minimum);
-ruleBlock->setDisjunction(new fl::Maximum);
-ruleBlock->setActivation(new fl::Minimum);
-ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is baja then velocidad is lenta", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is media then velocidad is lenta", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is alta then velocidad is lenta", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is baja then velocidad is media", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is media then velocidad is media", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is alta then velocidad is lenta", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is baja then velocidad is rapida", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is media then velocidad is rapida", engine));
-ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is alta then velocidad is media", engine));
-engine->addRuleBlock(ruleBlock);
+    fl::RuleBlock* ruleBlock = new fl::RuleBlock;
+    ruleBlock->setEnabled(true);
+    ruleBlock->setName("");
+    ruleBlock->setConjunction(new fl::Minimum);
+    ruleBlock->setDisjunction(new fl::Maximum);
+    ruleBlock->setActivation(new fl::Minimum);
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is baja then velocidad is baja", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is media then velocidad is baja", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is frio and humedad is alta then velocidad is media", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is baja then velocidad is media", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is media then velocidad is media", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is templado and humedad is alta then velocidad is alta", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is baja then velocidad is alta", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is media then velocidad is media", engine));
+    ruleBlock->addRule(fl::Rule::parse("if temperatura is caliente and humedad is alta then velocidad is alta", engine));
+    engine->addRuleBlock(ruleBlock);
+
 
 
        engine->configure("Minimum", "Maximum", "Minimum", "Maximum", "Centroid");
@@ -86,31 +96,37 @@ engine->addRuleBlock(ruleBlock);
        if (not engine->isReady(&status))
            throw Exception("Engine not ready. "
                "The following errors were encountered:\n" + status, FL_AT);
-    QString humedad = ui->humedad->text();
+
+       //Se almacenan los valores ingresados en la caja de texto
+       QString humedad = ui->humedad->text();
        QString temperatura = ui->temperatura->text();
 
+       //Se ingresan los valores a humedad y temperatura
        inputVariable1->setInputValue(temperatura.toFloat());
        inputVariable2->setInputValue(humedad.toFloat());
 
+       //se procesa la informacion
        engine->process();
-//       FL_LOG( "Power.output = " << Op::str(outputVariable->g ));
 
-
+        //se adquiere la salida despues de fuzzificarse
         float velocidad = outputVariable->getOutputValue();
-      //  FL_LOG( "Power.output = " << velocidad);
 
-        if( velocidad < 350 )
-            ui->Print_Velocidad->setText(" Baja");
+        ostr << velocidad;
 
-        else if(velocidad < 650)
-            ui->Print_Velocidad->setText(" Media");
+        QString texto = QString::number(velocidad);
 
-        else if(velocidad < 1001)
-            ui->Print_Velocidad->setText(" Alta");
+        //Comparar la velocidad para mostrarla
+        if( velocidad < 1260 )
+            ui->Print_Velocidad->setText( "Baja  -> " + texto);
+
+        else if(velocidad < 2300)
+            ui->Print_Velocidad->setText(" Media -> " + texto);
+
+        else if(velocidad < 3600)
+            ui->Print_Velocidad->setText(" Alta -> " + texto);
 
         else
             ui->Print_Velocidad->setText("Los valores no son correctos");
-      //  ui->Print_Velocidad->setText(QString::number(velocidad));
 
 }
 
